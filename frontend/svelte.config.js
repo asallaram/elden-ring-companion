7ea7fe2a020d4e1b7cf4d7df133029aed5705c64
Aneesh Sallaram <sallaram@unc.edu>
2026-01-01 20:36:25 -0500

    200. html

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -8,9 +8,9 @@ const config = {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: 'index.html',
+			fallback: '200.html',
 			precompress: false,
-			strict: true
+			strict: false
 		})
 	}
 };
